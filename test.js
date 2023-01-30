db3297905799868809b6e40e8aba2be9179cf7f3
Juan Castro Arancibia <juan.castro.arancibia@gmail.com>
2023-01-30 13:29:39 -0300

    refactor: deletes unused test file

--- test.js
+++
@@ -1,7 +0,0 @@
-const getCardExpire = () => {
-  const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
-  const year = (date.getFullYear() + 5).toString().substring(2)
-  return `${month}/${year}`
-}
-
-console.log(getCardExpire())
