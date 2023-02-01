b581bd0588c8b70aeb604653f836da7d37aa7668
Juan Castro Arancibia <juan.castroarancibia@naranja.com>
2023-02-01 18:59:53 -0300

    fix: fixes typo in topic arn

--- src/create-client/service/publishClientCreated.js
+++ src/create-client/service/publishClientCreated.js
@@ -3,7 +3,7 @@ const { publish } = require('@ranty/nbased/service/downstream/sns')
 const publishNewClient = async (client) => {
   publish({
     Message: client,
-    TopicArn: process.env.CLIENT_CREATED_TOPIC_ARN
+    TopicArn: process.env.CLIENT_CREATED_TOPIC
   })
 }
 
