37a1f6bc8043234f0bb69319c217dddb24bed8ec
Juan Castro Arancibia <juan.castroarancibia@naranja.com>
2023-02-01 16:56:17 -0300

    fix: Fixes serverless config typo

--- src/create-client/domain/createClient.js
+++ src/create-client/domain/createClient.js
@@ -1,13 +1,15 @@
-const { ValidateCreateClientInput } = require('../schema/createClientInput');
-const { createClient } = require('../service/getExample');
+const { ValidateCreateClientInput } = require('../schema/createClientInput')
+const { createClient } = require('../service/createClient')
+const { publishNewClient } = require('../service/publishClientCreated')
 
 module.exports = async (commandPayload, commandMeta) => {
-  new ValidateCreateClientInput(commandPayload, commandMeta);
+  new ValidateCreateClientInput(commandPayload, commandMeta)
 
-  await createClient(commandPayload);
+  await createClient(commandPayload)
+  await publishNewClient(commandPayload)
 
   return {
     status: 200,
     body: JSON.stringify('Usuario creado correctamente'),
-  };
-};
+  }
+}
